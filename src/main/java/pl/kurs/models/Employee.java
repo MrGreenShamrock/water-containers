a9080488e5a1221dcd87b7f5e255b3d234b2a869
Michal Sloniewski <michal.sloniewski@smartcodecreations.com>
2025-12-03 17:59:15 +0100

    Added Employee class

---
+++ src/main/java/pl/kurs/models/Employee.java
@@ -0,0 +1,28 @@
+package pl.kurs.models;
+
+public class Employee {
+
+    private String firstName;
+    private String lastName;
+    public double salary;
+
+    public Employee(String firstName, String lastName, double salary) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.salary = salary;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+
+
+
+
+}
